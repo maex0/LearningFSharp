a6122f4234ca4b9733a05242b761bf258683941c
maex0 <maxigobbel@googlemail.com>
2021-01-06 14:11:21 +0100

    Capstone 2 preparation

---
+++ MyFirstFSharpProject/Capstone2/Experimental.fsx
@@ -0,0 +1,16 @@
+﻿open System
+
+type Customer = 
+    { Firstname : string
+      Lastname : string }
+
+type Account =
+    { AccountId : Guid
+      Balance : decimal
+      Owner : Customer}
+     
+let acc = { AccountId=Guid.Empty; Balance=13.12M; Owner= { Firstname="John"; Lastname="Doe" } }
+
+/// Deposites an amount into an account
+let deposit (amount:decimal) (account:Account) : Account =
+    {AccountId = Guid.Empty; Owner={Firstname="Sam"; Lastname="Test"}; Balance=10M}
